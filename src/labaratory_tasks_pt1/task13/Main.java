0fc2b4d501ea18cbf6fe61dd716ff5672eb09a17
antonnotdead <oneusergav@gmail.com>
2023-08-14 22:56:14 +0300

    13.08.23

--- src/labaratory_tasks_pt1/task13/Main.java
+++ src/labaratory_tasks_pt1/task13/Main.java
@@ -6,8 +6,10 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), m = sc.nextInt();
-        int mas[][] = new int[n][m];
-        int temp_index;
+        int[][] mas = new int[n][m];
+        int max_index = 0;
+        int min_index = m-1;
+        int temp;
         for (int i = 0; i < n; i++){
             for (int j = 0; j < m; j++){
                 mas[i][j] = (int)(Math.random()*101)-50;
@@ -15,13 +17,32 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println();
         for (int i = 0; i < n; i++){
             for (int j = 0; j<m-1; j++){
-                if (mas[i][j] < mas[i][j+1]){
-                    temp_index = j+1;
+                if (mas[i][max_index] < mas[i][j+1]){
+                    max_index = j+1;
                 }
             }
+            temp = mas[i][max_index];
+            mas[i][max_index] = mas[i][0];
+            mas[i][0] = temp;
+            max_index = 0;
+            for (int k = m-1; k >= 0; k--){
+                if (mas[i][min_index] > mas[i][k-1]){
+                    min_index = k;
+                }
+            }
+            temp = mas[i][min_index];
+            mas[i][min_index] = mas[i][0];
+            mas[i][0] = temp;
+            min_index = m-1;
         }
-
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                System.out.print(mas[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
