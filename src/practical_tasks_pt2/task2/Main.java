c91ed196e9b0e5316b52f380994c3b4064936eeb
antonnotdead <oneusergav@gmail.com>
2023-08-15 16:34:39 +0300

    14.08.2023

--- src/practical_tasks_pt2/task2/Main.java
+++ src/practical_tasks_pt2/task2/Main.java
@@ -1,7 +1,5 @@
 package practical_tasks_pt2.task2;
 
-import practical_tasks_pt2.task1.Student;
-
 public class Main {
     public static void main(String[] args){
         Student ivanov = new Student();
