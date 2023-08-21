528a104bf02b5fb65eed358ca994ba23da1c3169
antonnotdead <oneusergav@gmail.com>
2023-08-21 17:25:00 +0300

    20.08.2023

--- src/practical_tasks_pt2/task14/ArrayString.java
+++ src/practical_tasks_pt2/task14/ArrayString.java
@@ -6,7 +6,7 @@ public class ArrayString {
     public static void main(String[] args){
         ArrayString slova = new ArrayString(5);
 //        slova.sort();
-        slova.showleght();
+        slova.showlenght();
     }
     private String[] strings;
     public ArrayString(int N){
@@ -53,7 +53,7 @@ public class ArrayString {
         }
     }
 
-    public void showleght(){
+    public void showlenght(){
         for (int i = 0; i < strings.length; i++) {
             System.out.println(i+1 + ". " + strings[i].length());
         }
@@ -62,5 +62,38 @@ public class ArrayString {
         for (int i = f; i < l; i++){
             System.out.println(strings[i]);
         }
+    }
+
+    public void maxNumber(){
+        int[] maxIntValues = new int[strings.length];
+        for (int i = 0; i < strings.length; i++){
+            StringBuilder digits = new StringBuilder();
+            for (int j = 0; j < strings[i].length(); j++){
+                char chrs = strings[i].charAt(j);
+                if (Character.isDigit(chrs))
+                    digits.append(chrs);
+            }
+            char maxStringValue = '0';
+            for (int k = 0; k < digits.length(); k++){
+                if (digits.charAt(k) > maxStringValue){
+                    maxStringValue = digits.charAt(k);
+                }
+            }
+            maxIntValues[i] = maxStringValue - '0';
+        }
+        int maxNumber = maxIntValues[0];
+        int maxIndex = 0;
+        for (int i = 1; i < maxIntValues.length; i++){
+            if (maxNumber < maxIntValues[i])
+                maxIndex = i;
+        }
+        System.out.println(strings[maxIndex]);
+    }
+    public void deleteUpper(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter index of string: ");
+        int index = sc.nextInt();
+        strings[index] = strings[index].replaceAll("[A-Z]", "");
+        System.out.println("Changed line: " + strings[index]);
     }
 }
