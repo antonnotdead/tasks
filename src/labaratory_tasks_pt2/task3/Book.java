83c2c5002a56b8eca57083d3b33387e17d5781b5
antonnotdead <oneusergav@gmail.com>
2023-09-29 00:46:41 +0300

    29.09.2023 00.46

--- src/labaratory_tasks_pt2/task3/Book.java
+++ src/labaratory_tasks_pt2/task3/Book.java
@@ -1,5 +1,8 @@
 package labaratory_tasks_pt2.task3;
 
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class Book {
     private  String author;
     private String bookName;
@@ -21,13 +24,38 @@ public class Book {
     public void setBookName(String bookName) {
         this.bookName = bookName;
     }
+    public void setBookName() {
+        Scanner sc = new Scanner(System.in);
+        this.bookName = sc.nextLine();
+    }
     public void setAuthor(String author) {
         this.author = author;
+    }
+    public void setAuthor() {
+        Scanner sc = new Scanner(System.in);
+        this.author = sc.nextLine();
     }
     public void setPages(int pages) {
         this.pages = pages;
     }
+
+    public void setPages() {
+        Scanner sc = new Scanner(System.in);
+        this.pages = sc.nextInt();
+    }
     public void setPublishingYear(String publishingYear) {
         this.publishingYear = publishingYear;
+    }
+    public void setPublishingYear() {
+        Scanner sc = new Scanner(System.in);
+        this.publishingYear = sc.nextLine();
+    }
+
+    public void getFullBookInfo(String bookName,Book ... books) {
+        for (Book book : books) {
+            if (book.bookName.equals(bookName)){
+                System.out.println(book);
+            }
+        }
     }
 }
